d2be001b6bc9a96c34bcbc455af5d18163dd935b
Lewis England <lewis2004@hotmail.com>
2020-03-26 22:57:24 +0000

    [feat] Add Google Maps search. Add Modal for COVID diagnosis
    Google Maps search integrated for identifying locations. Lat &
     long are available via this API result
    
    Started an info modal for clicking the "I have COVID-19" button

--- Frontend/src/components/UserOps.vue
+++ Frontend/src/components/UserOps.vue
@@ -1,13 +1,26 @@
 <template>
   <div id='user-ops-container'>
-    <b-button variant="danger" class="ml-2 mr-2">I Have COVID-19</b-button>
-    <b-button variant="outline-primary" class="ml-2 mr-2" @click="onUserLogout">Log Out</b-button>
+    <b-button variant="danger" class="ml-2 mr-2" @click="$bvModal.show('covid-modal')">
+      I Have COVID-19
+    </b-button>
+    <b-button variant="outline-primary" class="ml-2 mr-2" @click="onUserLogout">
+      Log Out
+    </b-button>
+
+    <b-modal id="covid-modal">
+      <CovidModal></CovidModal>
+    </b-modal>
   </div>
 </template>
 
 <script>
+import CovidModal from '@/components/CovidModal.vue'
+
 export default {
   name: 'UserOps',
+  components :{
+    CovidModal
+  },
   methods: {
     onUserLogout (evt) {
       evt.preventDefault()
