b352bf7b9f0e55a9ef9552091ffada3a7db8a737
Lewis England <lewis2004@hotmail.com>
2020-03-26 17:27:24 +0000

    [feat] Make the Log Out button functional

--- Frontend/src/components/UserOps.vue
+++ Frontend/src/components/UserOps.vue
@@ -1,6 +1,18 @@
 <template>
   <div id='user-ops-container'>
     <b-button variant="danger" class="ml-2 mr-2">I Have COVID-19</b-button>
-    <b-button variant="outline-primary" class="ml-2 mr-2">Log Out</b-button>
+    <b-button variant="outline-primary" class="ml-2 mr-2" @click="onUserLogout">Log Out</b-button>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'UserOps',
+  methods: {
+    onUserLogout (evt) {
+      evt.preventDefault()
+      this.$store.dispatch('storeUserAuth', false)
+    }
+  }
+}
+</script>
