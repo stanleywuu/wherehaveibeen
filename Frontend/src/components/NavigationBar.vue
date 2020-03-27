3703d59ca5f6dba9313afb69fab3ece4d2162c21
Lewis England <lewis2004@hotmail.com>
2020-03-27 23:04:45 +0000

    [style] Re-style the navigatoin bar and add branding

--- Frontend/src/components/NavigationBar.vue
+++ Frontend/src/components/NavigationBar.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="nav-bar-container" v-show="userLoggedIn" class="mb-5">
-    <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar toggleable type="dark" variant="dark">
+      <b-navbar-brand>Where Have I Been?</b-navbar-brand>
       <b-navbar-nav class="ml-auto">
         <UserOps></UserOps>
       </b-navbar-nav>
