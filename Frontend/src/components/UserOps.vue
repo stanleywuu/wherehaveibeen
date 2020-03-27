acc0886a250f84571663611d2fab74e12291667d
Lewis England <lewis2004@hotmail.com>
2020-03-27 23:03:57 +0000

    [refactor] Edit the user ops buttons to suit the navigation bar style

--- Frontend/src/components/UserOps.vue
+++ Frontend/src/components/UserOps.vue
@@ -1,9 +1,9 @@
 <template>
   <div id='user-ops-container'>
-    <b-button variant="danger" class="ml-2 mr-2" @click="$bvModal.show('covid-modal')">
+    <b-button id='covid-report-btn' variant="danger" class="ml-2 mr-2" @click="$bvModal.show('covid-modal')">
       I Have COVID-19
     </b-button>
-    <b-button variant="outline-primary" class="ml-2 mr-2" @click="onUserLogout">
+    <b-button id='logout-btn' variant="outline-light" class="ml-2 mr-2" @click="onUserLogout">
       Log Out
     </b-button>
 
@@ -29,3 +29,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+#covid-report-btn {
+  color: #343a40 !important;
+}
+</style>
