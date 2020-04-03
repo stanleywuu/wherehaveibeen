308a4397c17809d66eb8fdb7165221a5eca9a15a
Lewis England <lewis2004@hotmail.com>
2020-04-03 15:57:00 +0100

    Linting code. Add screenshot for submissin

--- Frontend/src/components/CheckInHistory.vue
+++ Frontend/src/components/CheckInHistory.vue
@@ -28,9 +28,9 @@
             <span v-for="detail in loc.details"
               :key="detail.VisitId">
               <p>Someone with symptoms was here at:</p>
-              <p>Checked in: {{dateFormatter(detail.checkIn)}}</p>
-              <p>Checked out: {{dateFormatter(detail.checkOut)}}</p>
-              <p>{{detail.distanceInKm}} KM</p>
+              <p>Checked in: {{ dateFormatter(detail.checkIn) }}</p>
+              <p>Checked out: {{ dateFormatter(detail.checkOut) }}</p>
+              <p>{{ detail.distanceInKm }} KM</p>
             </span>
             </div> 
         </b-col>
@@ -38,65 +38,32 @@
           <b-card-body>
             <b-row no-gutters>
               <b-col class="history-text">
-                <h4>
-                  {{loc.PlaceName}}
-                </h4>
+                <h4>{{ loc.PlaceName }}</h4>
               </b-col>
               <b-col sm="3" class="risk-label">
                 <span v-if="loc.AtRisk" class="at-risk">
-                  <font-awesome-icon icon="biohazard" />
-                  At Risk
+                  <font-awesome-icon icon="biohazard" /> At Risk
                 </span>
                 <span class="risky" v-if="!loc.AtRisk && loc.RiskyInteractions > 0">
-                  <font-awesome-icon icon="hand-sparkles" />
-                  Potential Risk 
+                  <font-awesome-icon icon="hand-sparkles" /> Potential Risk 
                   <b-button id='covid-interaction-btn' class="ml-1 mr-1"
                     @click="getDetails(loc)">
-                    {{loc.RiskyInteractions}} risky Encounters
+                    {{ loc.RiskyInteractions }} risky encounters
                   </b-button>
-                  
                 </span>
                 <span v-if="!loc.AtRisk && loc.RiskyInteractions == 0">
-                  <font-awesome-icon icon="hand-sparkles" />
-                  Low Risk
+                  <font-awesome-icon icon="hand-sparkles" /> Low Risk
                 </span>
               </b-col>
             </b-row>
-            <div class="history-text">
-              {{ loc.Address }}
-            </div>
-            <div class="history-text">
-              {{ dateFormatter(loc.CheckIn) }}
-            </div>
+            <div class="history-text">{{ loc.Address }}</div>
+            <div class="history-text">{{ dateFormatter(loc.CheckIn) }}</div>
           </b-card-body>
         </b-col>
       </b-row>
     </b-card>
   </div>
 </template>
-
-<style>
-  .history-text {
-    text-align: left;
-  }
-  .risk-label {
-    display: flex;
-  }
-  .risky-text p
-  {
-    text-align:left;
-    margin-top:2pt;
-    margin-bottom:2px;
-  }
-  .at-risk {
-    color: red;
-    font-weight: bold;
-  }
-  .risky {
-    color:teal;
-    font-weight: bold;
-  }
-</style>
 
 <script>
 export default {
@@ -144,25 +111,18 @@ export default {
       }
     },
     dateFormatter (datetime) {
-      let epoch = new Date(Date.parse(datetime))
-      return epoch
+      return new Date(Date.parse(datetime))
     },
-    getDetails(loc)
-    {
-      if (loc.hasDetail)
-      {
-        this.$set(loc, 'hasDetail', !loc.hasDetail);
+    getDetails (loc) {
+      if (loc.hasDetail) {
+        this.$set(loc, 'hasDetail', !loc.hasDetail)
       }
-
-      if (loc.data && loc.data.length > 0)
-      {
-        return; //already have data
+      if (loc.data && loc.data.length > 0) {
+        return //already have data
       }
-
       this.$http.get(this.api.endpoint + '/visit/risk?visitId=' + loc.VisitId)
         .then(response => {
-          if (response.data && response.data.length > 0)
-          {
+          if (response.data && response.data.length > 0) {
             loc.hasDetail = true
             this.$set(loc, 'details', response.data)
           }
@@ -174,3 +134,26 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .history-text {
+    text-align: left;
+  }
+  .risk-label {
+    display: flex;
+  }
+  .risky-text p
+  {
+    text-align:left;
+    margin-top:2pt;
+    margin-bottom:2px;
+  }
+  .at-risk {
+    color: red;
+    font-weight: bold;
+  }
+  .risky {
+    color:teal;
+    font-weight: bold;
+  }
+</style>
