95642b50ffd1b0b57720936d30003222395cf86a
Lewis England <lewis2004@hotmail.com>
2020-04-14 22:48:11 +0100

    Installing the PWA plugin to auto-gen assets and config for PWA App

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -6,6 +6,7 @@ import BootstrapVue from 'bootstrap-vue'
 import * as VueGoogleMaps from "vue2-google-maps"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {
+  faBars,
   faBiohazard,
   faHandSparkles,
   faVirus,
@@ -14,10 +15,12 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
 
+library.add(faBars)
 library.add(faBiohazard)
 library.add(faHandSparkles)
 library.add(faVirus)
