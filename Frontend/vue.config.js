8ddd7353af9b26027662a3294e7e5fe88a1b3323
Lewis England <lewis2004@hotmail.com>
2020-04-14 22:48:35 +0100

    Adding a config file for the PWA app to set name and color scheme

---
+++ Frontend/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pwa: {
+    name: 'Where Have I Been?',
+    themeColor: '#2c3e50',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black'
+  }
+}
