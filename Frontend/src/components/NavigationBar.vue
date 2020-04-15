b5deaecbdf091349112d352da799f2d6ffbeabaa
Lewis England <lewis2004@hotmail.com>
2020-04-15 16:03:52 +0100

    Added split ops button for recovery / not COIVD to the nav bar

--- Frontend/src/components/NavigationBar.vue
+++ Frontend/src/components/NavigationBar.vue
@@ -17,8 +17,9 @@
             I Have Symptoms
           </b-button>
           <b-dropdown v-if="userLoggedIn && userReportStatus" split
-            text="I Have Recovered" variant="info" class="ml-2 mr-2">
-            <b-dropdown-item href="#">I didn't have COVID</b-dropdown-item>
+            text="I Have Recovered" @click="onReportRecovered()"
+            variant="info" class="ml-2 mr-2">
+            <b-dropdown-item @click="onReportNotCorona()">I didn't have COVID</b-dropdown-item>
           </b-dropdown>
           <b-nav-item-dropdown text="Account" right v-show="userLoggedIn">
             <b-dropdown-item @click="$emit('displayHistory')">History</b-dropdown-item>
@@ -30,8 +31,8 @@
     </b-navbar>
     <b-modal id="covid-modal" ref="covid-modal" hide-header hide-footer>
       <CovidModal
-        @confirmReport="confirmReport()"
-        @cancelReport="cancelReport()">
+        @confirmReport="onReportCorona()"
+        @cancelReport="onReportCancel()">
       </CovidModal>
     </b-modal>
   </div>
@@ -66,16 +67,31 @@ export default {
       this.$store.dispatch('storeUserAuth', undefined)
       this.$store.dispatch('storeUserId', undefined)
     },
-    confirmReport() {
+    onReportCancel () {
       this.$refs['covid-modal'].hide()
-      let userToken = this.$store.getters.getUserToken
+    },
+    onReportCorona () {
+      this.$refs['covid-modal'].hide()
+      this.$store.dispatch('storeUserReportStatus', true)
+      this.onReportSubmit('corona', true)
+    },
+    onReportRecovered () {
+      this.$store.dispatch('storeUserReportStatus', false)
+      this.onReportSubmit('recovered', false)
+    },
+    onReportNotCorona () {
+      this.$store.dispatch('storeUserReportStatus', false)
+      let apiPath = 'notcorona?userId=' + this.$store.getters.getUserId
+      this.onReportSubmit(apiPath, false)
+    },
+    onReportSubmit (endpoint, userAtRisk) {
       let requestData = {
         userId: this.$store.getters.getUserId,
-        IsAtRisk: true
+        IsAtRisk: userAtRisk
       }
-      this.$http.post(this.api.endpoint + '/membership/corona',
+      this.$http.post(this.api.endpoint + '/membership/' + endpoint,
         requestData,
-        {headers: { Authorization: "Bearer " + userToken }}
+        {headers: { Authorization: "Bearer " + this.$store.getters.getUserToken }}
       )
       .then(response => {
         this.api.responses.push(response)
